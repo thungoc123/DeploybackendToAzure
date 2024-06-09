1fa6028c6ab3dcfd14a9c942e1e1c5f122e67c61
thungoc123 <92897465+thungoc123@users.noreply.github.com>
2024-06-09 19:33:17 +0700

    Try to deploy

--- src/main/java/com/example/SWP391/security/CustomeAuthenProvider.java
+++ src/main/java/com/example/SWP391/security/CustomeAuthenProvider.java
@@ -29,3 +29,59 @@ public class CustomeAuthenProvider implements AuthenticationProvider {
         return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
+//package com.example.SWP391.security;
+//
+//import com.example.SWP391.entity.Account;
+//import com.example.SWP391.repository.AccountRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collections;
+//
+//@Service
+//public class CustomeAuthenProvider implements AuthenticationProvider {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(); // Sử dụng BCrypt để mã hóa mật khẩu
+//    }
+//    @Autowired
+//    private AccountRepository accountRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        String email = (String) authentication.getPrincipal();
+//        String password = (String) authentication.getCredentials();
+//
+//        // Tìm tài khoản dựa trên email
+//        Account account = accountRepository.findByEmail(email);
+//        if (account == null) {
+//            throw new BadCredentialsException("Invalid email or password");
+//        }
+//
+//        // Kiểm tra mật khẩu đã mã hóa
+//        if (!passwordEncoder.matches(password, account.getPassword())) {
+//            throw new BadCredentialsException("Invalid email or password");
+//        }
+//
+//        // Trả về một UsernamePasswordAuthenticationToken với quyền
+//        return new UsernamePasswordAuthenticationToken(email, password,
+//                Collections.singletonList(new SimpleGrantedAuthority("USER")));
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authentication) {
+//        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+//    }
+//}
